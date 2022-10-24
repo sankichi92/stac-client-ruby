880b231004cda296dbc23bbc65c63331d03d7456
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2022-10-24 22:16:29 +0900

    bundle gem stac-client -t rspec

---
+++ spec/stac/client_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Stac::Client do
+  it "has a version number" do
+    expect(Stac::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
