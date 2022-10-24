00dce28c4bf2de1820870be9728e3db86f008092
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2022-10-24 22:22:46 +0900

    Remove a default failing test

--- spec/stac/client_spec.rb
+++ spec/stac/client_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Stac::Client do
   it "has a version number" do
     expect(Stac::Client::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
